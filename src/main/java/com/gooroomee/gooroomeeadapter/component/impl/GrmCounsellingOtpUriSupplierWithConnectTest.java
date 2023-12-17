bc75ab43100ad8571a19ad56c65dff99b1c9256f
jinwelcome1015 <jinwelcome1015@gmail.com>
2023-12-17 16:08:29 +0900

    commit

--- src/main/java/com/gooroomee/gooroomeeadapter/component/impl/GrmCounsellingOtpUriSupplierWithConnectTest.java
+++ src/main/java/com/gooroomee/gooroomeeadapter/component/impl/GrmCounsellingOtpUriSupplierWithConnectTest.java
@@ -20,7 +20,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Component
 @Slf4j
 public class GrmCounsellingOtpUriSupplierWithConnectTest implements GrmCounsellingOtpUriSupplier {
@@ -28,26 +27,26 @@ public class GrmCounsellingOtpUriSupplierWithConnectTest implements GrmCounselli
 	/** MVC시스템입장URI를 발행해주는 서비스의 URI */
 	@Value(value = "${mvc.entry-uri-issue-service.uri_1}")
 	private String uriOfMvcEntryUriIssueService_1;
-	
+
 	/** MVC시스템입장URI를 발행해주는 서비스의 URI */
 	@Value(value = "${mvc.entry-uri-issue-service.uri_2}")
 	private String uriOfMvcEntryUriIssueService_2;
-	
+
 	@Override
 	public String getFirstConnectableUri() throws IOException {
 		List<IpPortInfo> ipPortInfosOfMvcEntryUriIssueService = this.getIpPortInfosOfMvcEntryUriIssueService();
 		List<IpPortInfo> reorderedLocalFirstIpPortInfos = this.reorderLocalFirst(ipPortInfosOfMvcEntryUriIssueService);
-		
+
 		String firstConnectableUri = null;
-		
+
 		for (int i = 0; i < reorderedLocalFirstIpPortInfos.size(); i++) {
 			IpPortInfo ipPortInfo = reorderedLocalFirstIpPortInfos.get(i);
-			
+
 			String ip = ipPortInfo.getIp();
 			int port = ipPortInfo.getPort();
-			
+
 			boolean isConnectable = false;
-			
+
 			Socket socket = new Socket();
 			try {
 				socket.connect(new InetSocketAddress(ip, port));
@@ -57,95 +56,92 @@ public class GrmCounsellingOtpUriSupplierWithConnectTest implements GrmCounselli
 				isConnectable = socket.isConnected();
 				socket.close();
 			}
-			
-			if(isConnectable) {
+
+			if (isConnectable) {
 				firstConnectableUri = ipPortInfo.getUri();
 				break;
 			} else {
 				continue;
 			}
 		}
-		
-		if(firstConnectableUri == null) {
+
+		if (firstConnectableUri == null) {
 			throw new RuntimeException(String.format("%s 중, 연결가능한 URI가 없습니다.", reorderedLocalFirstIpPortInfos));
 		}
-		
+
 		return firstConnectableUri;
 	}
-	
-	
-	private List<IpPortInfo> reorderLocalFirst(List<IpPortInfo> ipPortInfos){
+
+	private List<IpPortInfo> reorderLocalFirst(List<IpPortInfo> ipPortInfos) {
 		String localIpAddress = NetworkUtil.getLocalIpAddress();
-		
+
 		for (int i = 0; i < ipPortInfos.size(); i++) {
 			IpPortInfo ipPortInfo = ipPortInfos.get(i);
-			if(localIpAddress.equals(ipPortInfo.getIp())) {
+			if (localIpAddress.equals(ipPortInfo.getIp())) {
 				ipPortInfos.remove(i);
 				ipPortInfos.add(0, ipPortInfo);
 			}
 		}
-		
+
 		log.info("[reorderLocalFirst] : {}", ipPortInfos);
-		
+
 		return ipPortInfos;
 	}
-	
-	
-	private List<IpPortInfo> getIpPortInfosOfMvcEntryUriIssueService(){
+
+	private List<IpPortInfo> getIpPortInfosOfMvcEntryUriIssueService() {
 		IpPortInfo ipPortInfo_1 = this.getIpPortInfo(uriOfMvcEntryUriIssueService_1);
 		IpPortInfo ipPortInfo_2 = this.getIpPortInfo(uriOfMvcEntryUriIssueService_2);
-		
+
 		List<IpPortInfo> ipPortInfoList = new ArrayList<>();
 		ipPortInfoList.add(ipPortInfo_1);
 		ipPortInfoList.add(ipPortInfo_2);
-		
+
 		return ipPortInfoList;
 	}
 
-	
 	private IpPortInfo getIpPortInfo(String uri) {
 		String tokenOfHttp = "http://";
 		String tokenOfHttps = "https://";
-		
+
 		String pattern1 = String.format("^(%s|%s)", tokenOfHttp, tokenOfHttps);
 		String pattern2 = "/.*";
-		
+
 		String temp = uri;
 		temp = temp.replaceAll(pattern1, "");
 		temp = temp.replaceAll(pattern2, "");
-		
+
 		String[] ipPort = temp.split(":");
-		
+
 		String ip;
 		int port;
-		
-		if(ipPort.length == 1) {
+
+		if (ipPort.length == 1) {
 			ip = ipPort[0];
 			boolean isHttp = Pattern.matches(String.format("^%s", tokenOfHttp), uri);
 			boolean isHttps = Pattern.matches(String.format("^%s", tokenOfHttps), uri);
-			if(isHttp) {
+			if (isHttp) {
 				port = 80;
-			}else if(isHttps) {
+			} else if (isHttps) {
 				port = 443;
-			}else {
+			} else {
 				throw new RuntimeException("URI 의 프로토콜이 HTTP도, HTTPS 도 아닙니다.");
 			}
-		}else if(ipPort.length == 2) {
+		} else if (ipPort.length == 2) {
 			ip = ipPort[0];
 			port = Integer.parseInt(ipPort[1]);
-		}else {
+		} else {
 			throw new RuntimeException("IP 와 PORT 를 추출하는 과정에서 예외가 발생했습니다.");
 		}
-		
+
 		IpPortInfo ipPortInfo = new IpPortInfo(uri, ip, port);
-		
+
 		return ipPortInfo;
 	}
-	
+
 	@Getter
 	@Setter
 	@AllArgsConstructor
-	public static class IpPortInfo {
+	private static class IpPortInfo {
 		private String uri;
 		private String ip;
 		private int port;
